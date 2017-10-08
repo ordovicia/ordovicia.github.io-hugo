9ab5f04d6b80c5037116529d5d931b6e4a85f036
Hidehito Yabuuchi <hdht.ybuc@gmail.com>
2017-10-08 23:44:13 +0900

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,8 @@
 
 set -e
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
 hugo
 cd public
-git add --all
-
-msg="rebuilding site $(date)"
-if [ $# -eq 1 ]; then
-    msg="$1"
-fi
-git commit -m "$msg"
+msg=${1:-"rebuilding site $(date)"}
+git commit -am "$msg"
 git push origin master
